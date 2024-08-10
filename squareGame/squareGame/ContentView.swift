45f3572578b724ad09304c9704f9f70faf4b4d59
COBSCCOMPY4231P-15 <cobsccompy4231p-15@NIBM-LAB04-PC02.local>
2024-08-10 14:28:11 +0530

    basis

---
+++ squareGame/squareGame/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  squareGame
+//
+//  Created by COBSCCOMPY4231P-15 on 2024-08-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Square Game!")
+                .font(.caption)
+                
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ squareGame/squareGame/squareGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  squareGameApp.swift
+//  squareGame
+//
+//  Created by COBSCCOMPY4231P-15 on 2024-08-10.
+//
+
+import SwiftUI
+
+@main
+struct squareGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
